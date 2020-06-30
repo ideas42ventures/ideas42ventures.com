46061e174bbdf5c9f5ba69da51eecc567a4321c2
Tyler Gaw <me@tylergaw.com>
2020-06-30 18:08:07 -0400

    Adds titlecase helper

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,18 @@
 module.exports = function (conf) {
+  conf.addFilter("makeTitleCase", (str) =>
+    str.replace(
+      /\w\S*/g,
+      (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+    )
+  );
+
   return {
     dir: {
       input: "src",
       output: "dist",
       includes: "includes",
       layouts: "layouts",
+      data: "data",
     },
   };
 };
