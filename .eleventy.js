6eaf87d0ea06feb40318105eb3fba8c9b88c4edd
Tyler Gaw <me@tylergaw.com>
2020-09-15 10:20:44 -0400

    Styles Ventures for Shared Prosperity Pages (#40)
    * Styles the Ventures for Shared Prosperity pages
    * Moves `cohort-nav` to a macro to allow args
    * Adds callout component
    * Adds new system colors
    * Adds more foundation typographic styles
    * Adds more foundation grid styles and util classes
    * Updates main nav links (this will still change)
    * Sets `img` sizes throughout to help CLS
    * Adds dateFormat shortcode
    * Adds headingAnchor shortcode
    * Makes hero top padding configurable with a custom prop
    * Removes no-longer used apply page and related styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,25 @@
-const pluginPWA = require("eleventy-plugin-pwa");
 const CleanCSS = require("clean-css");
+const { DateTime } = require("luxon");
+
+const slugify = (str) =>
+  str
+    .toLowerCase()
+    .replace(/[^\w\s]|_/g, "")
+    .replace(/\s+/g, "-");
 
 module.exports = function (conf) {
   conf.addFilter("cssmin", (code) => new CleanCSS({}).minify(code).styles);
+  conf.addShortcode("dateFormat", (dateStr, frmt = "DDD 'at' t ZZZZ") =>
+    DateTime.fromISO(dateStr).toFormat(frmt)
+  );
+  conf.addShortcode("headingAnchor", (level, text, id) => {
+    const finalId = id ? id : slugify(text);
+
+    return `<h${level} id="${finalId}">
+        <a class="header-anchor" href="#${finalId}">#</a>
+        ${text}
+      </h${level}>`;
+  });
 
   conf.setTemplateFormats([
     "html",
@@ -18,7 +35,6 @@ module.exports = function (conf) {
 
   conf.addPassthroughCopy("./src/manifest.webmanifest");
   conf.addWatchTarget("./src/**/*.css");
-  conf.addPlugin(pluginPWA);
 
   return {
     dir: {
