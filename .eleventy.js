19c347a5eeba0e4ea937ea189c3a4ec38c36fa5e
Tyler Gaw <me@tylergaw.com>
2020-09-14 21:55:05 -0400

    Adds headingAnchor shortcode and uses

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,25 @@
 const CleanCSS = require("clean-css");
 const { DateTime } = require("luxon");
 
+const slugify = (str) =>
+  str
+    .toLowerCase()
+    .replace(/[^\w\s]|_/g, "")
+    .replace(/\s+/g, "-");
+
 module.exports = function (conf) {
   conf.addFilter("cssmin", (code) => new CleanCSS({}).minify(code).styles);
   conf.addShortcode("dateFormat", (dateStr, frmt = "DDD 'at' t ZZZZ") =>
     DateTime.fromISO(dateStr).toFormat(frmt)
   );
+  conf.addShortcode("headingAnchor", (level, text, id) => {
+    const finalId = id ? id : slugify(text);
+
+    return `<h${level} id="${finalId}">
+        <a class="header-anchor" href="#${finalId}">#</a>
+        ${text}
+      </h${level}>`;
+  });
 
   conf.setTemplateFormats([
     "html",
