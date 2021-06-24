7f94218bea60aa0c0d11ef4221ecec3c80b5ad3b
Amanda Munoz <amanda@ideas42ventures.com>
2021-06-24 11:08:05 -0400

    Fix bug where the anchorReference was referring to the wrong element (the span)

--- src/js/celena.js
+++ src/js/celena.js
@@ -6,18 +6,22 @@ window.addEventListener("load", function () {
     for (var i = 0; i < myTabs.length; i++) {
       myTabs[i].classList.remove("active");
     }
+
     var clickedTab = tabClickEvent.currentTarget;
     clickedTab.classList.add("active");
     tabClickEvent.preventDefault();
+
     var myContentPanes = document.querySelectorAll(".tab-pane");
     for (i = 0; i < myContentPanes.length; i++) {
       myContentPanes[i].classList.remove("active");
     }
-    var anchorReference = tabClickEvent.target;
+
+    var anchorReference = clickedTab.firstChild; // only get the a element
     var activePaneId = anchorReference.getAttribute("href");
     var activePane = document.querySelector(activePaneId);
     activePane.classList.add("active");
   }
+
   for (i = 0; i < myTabs.length; i++) {
     myTabs[i].addEventListener("click", myTabClicks);
   }
